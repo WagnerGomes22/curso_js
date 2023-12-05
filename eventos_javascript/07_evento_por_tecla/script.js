40f29dd9553d41f9fcfb1312f9816caede3e7822
Wagner-tech3 <wagnergomesdossantosw5@gmail.com>
2023-12-05 13:03:43 -0300

    .

---
+++ eventos_javascript/07_evento_por_tecla/script.js
@@ -0,0 +1,14 @@
+window.addEventListener("keyup", function(e) {
+
+    if(e.key == "q") {
+        console.log("q")
+    } 
+
+});
+
+window.addEventListener("keyup", function(e) {
+
+    if(e.key == "Enter"){
+        console.log("enter")
+    }
+});
